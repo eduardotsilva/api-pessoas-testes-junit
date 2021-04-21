2d411ff7b724a80517bf8ce655b1ab75c9505dc8
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-21 20:44:13 -0300

    Outros recursos úteis do Mockito

--- src/test/java/com/eduardo/MockitoTests.java
+++ src/test/java/com/eduardo/MockitoTests.java
@@ -3,21 +3,32 @@ package com.eduardo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
 import java.util.List;
-
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTests {
+
+    @Mock
+    List<String> lista;
 
     @Test
     public void primeiroTesteMockito(){
-        List<String> lista = Mockito.mock(ArrayList.class);
-
         Mockito.when(lista.size() ).thenReturn(20);
 
-        int size = lista.size();
+        int size = 0;
+        if(true){
+          size = lista.size();
+          size = lista.size();
+        }
+//        Assertions.assertThat(size).isEqualTo(20);
+        //verifica se chamou método size da lista
+        Mockito.verify(lista, Mockito.times(2)).size();
 
-        Assertions.assertThat(size).isEqualTo(20);
+        //verifica se nunca foi executado
+//        Mockito.verify(lista, Mockito.never()).size();
     }
 }
